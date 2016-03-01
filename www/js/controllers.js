5e94fe3220f3b72adaddf52d0664e39cdd04db0d
Carmen Long <carmenlong@Carmens-MacBook-Pro.local>
2016-02-29 22:37:57 -0600

    adding broken app. :)

---
+++ www/js/controllers.js
@@ -0,0 +1,41 @@
+var app = angular.module('starter.controllers', []);
+
+app.controller('DashCtrl', function($scope) {});
+
+app.controller('tabsCtrl', function($scope) {
+  $scope.show=true;
+  $scope.clickedAccount = function() {
+    if($scope.show) {
+      $scope.show=false;
+    } else {
+      $scope.show=true;
+    }
+    console.log("Show is now " + $scope.show);
+  }
+});
+
+
+app.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+});
+
+app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+});
+
+app.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
